f768743b18cc378b8c35d524eba9146f4b348c36
ny-sujan-s <Sujan.suthakaran@noseryoung.ch>
2025-09-19 14:41:06 +0200

    create files

---
+++ src/design.c
@@ -0,0 +1,5 @@
+//
+// Created by sujan on 19.09.2025.
+//
+
+#include "design.h"
---
+++ src/design.h
@@ -0,0 +1,8 @@
+//
+// Created by sujan on 19.09.2025.
+//
+
+#ifndef TASCHENRECHNER_DESIGN_H
+#define TASCHENRECHNER_DESIGN_H
+
+#endif //TASCHENRECHNER_DESIGN_H
