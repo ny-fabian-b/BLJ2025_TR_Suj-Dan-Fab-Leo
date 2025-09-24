d2d3c7e51006aec41d8065f6398568b973195985
ny-sujan-s <Sujan.suthakaran@noseryoung.ch>
2025-09-24 09:47:27 +0200

    add basic operators to simple evaluation

--- src/simple_functions.c
+++ src/simple_functions.c
@@ -19,9 +19,23 @@ size_t find_operator(char op, Expression**expression, size_t len) {
 void evaluate_simple_functions(Expression**expression, size_t len) {
     for (size_t i = 0; i < len; i++) {
          size_t exp = find_operator('^',expression,len);
-        size_t exp = find_operator('*',expression,len);
+         size_t minus = find_operator('-',expression,len);
+         size_t plus = find_operator('+',expression,len);
+         size_t mult = find_operator('*',expression,len);
+         size_t div = find_operator('/',expression,len);
 
 
+        size_t j = minus;
+
+        j = mult;
+
+        if (minus > plus) {
+            j = plus;
+        }
+
+        if (mult > div) {
+            j = div;
+        }
     }
 
 }
