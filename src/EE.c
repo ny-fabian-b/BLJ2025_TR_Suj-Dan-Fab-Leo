c133faf1fe06108b261572e52418f380537cb42f
ny-daniel-l <daniel.lakatos@edu.tbz.ch>
2025-09-26 08:33:29 +0200

    EE fix v2

--- src/EE.c
+++ src/EE.c
@@ -8,21 +8,15 @@
     //char eingabe[50];
 
 //    printf("Bitte bewerte den Taschenrechner (0-5): ");
-  //  scanf("%49[^\n]", eingabe);
 
 //    if (strcmp(eingabe, "daniel on top") == 0) {
 //        printf("yh thats true \n");
 //    } else {
 //        int bewertung;
-//        if (sscanf(eingabe, "%d", &bewertung) == 1) {
 //            if (bewertung >= 0 && bewertung <= 5) {
 //                printf("Danke für deine Bewertung: %d von 5 Sternen\n", bewertung);
 //            } else {
-//                printf("Ungueltige Eingabe! Bitte Zahl zwischen 0 und 5 eingeben.\n");
-//            }
-//        } else {
-//            printf("Ungueltige Eingabe! Bitte Zahl oder 'daniel on top' eingeben.\n");
-//        }
+//                printf("Ungueltige Eingabe! Bitte Zahl zwischen 0 und 5 eingeben.\n")
 //    }
 
     return 0;
